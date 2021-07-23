ebfea6507b48ee53c24ae7ca146c963249a4625b
emiri <lilyyy.emll@gmail.com>
2021-07-23 17:46:25 +0900

    Add NewServlet.java,EncodingFilter.java

---
+++ src/controllers/NewServlet.java
@@ -0,0 +1,58 @@
+package controllers;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class NewServlet
+ */
+@WebServlet("/new")
+public class NewServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NewServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        EntityManager em = DBUtil.createEntityManager();
+
+     // Taskのインスタンスを生成
+        Task t = new Task();
+
+     // lの各フィールドにデータを代入
+        String content = "today's task";
+        t.setContent(content);
+
+        Timestamp currentTime = new Timestamp(System.currentTimeMillis());     // 現在の日時を取得
+        t.setCreated_at(currentTime);
+        t.setUpdated_at(currentTime);
+
+        // データベースに保存
+        em.getTransaction().begin();
+        em.persist(t);
+        em.getTransaction().commit();
+
+        // 自動採番されたIDの値を表示
+        response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+        em.close();
+     }
+}
+
